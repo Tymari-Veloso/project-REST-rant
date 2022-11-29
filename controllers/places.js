77e1ee3251bc8a57b44e1be576e98d81d8cb34aa
Tymari Veloso <tymariveloso@gmail.com>
2022-11-28 19:43:24 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,23 @@ const places = require ('../models/places.js')
 router.get('/',  function(req, res){
     res.render('places/index', {places})
 })
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
 
 router.post('/', (req, res) => {
     if (!req.body.pic) {
@@ -19,13 +36,5 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  
-  
-
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-
 
 module.exports = router
