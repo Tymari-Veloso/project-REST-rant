d938569e24fcfb8b1688f9d8f489522ef1fd697c
Tymari Veloso <tymariveloso@gmail.com>
2022-12-05 19:20:20 -0800

    added mongoose

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,11 +2,11 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFortmatted = data.places.map((place, index) => {
+    let placesFortmatted = data.places.map((place) => {
         return (
             <div className='col-sm-6'>
                 <h2>
-                    <a href={`/places/${index}`}>
+                    <a href={`/places/${place.id}`}>
                         {place.name}
                     </a>
                 </h2>
