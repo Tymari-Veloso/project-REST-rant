d559a00e8077b9e1ed6eaca4b33de23397d43631
Tymari Veloso <tymariveloso@gmail.com>
2022-12-16 14:40:46 -0800

    Added visable stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -16,10 +16,14 @@ function show (data) {
     let sumRating = data.place.comments.reduce ((tot, c)=> {
       return tot + c.stars
     }, 0)
-    let averageRating = sumRating / data.place.comments.length
+    let averageRating = Math.round(sumRating / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++){
+      stars += '⭐'
+    }
     rating =(
       <h3>
-        {Math.round(averageRating)} stars
+        {stars} stars
       </h3>
     )
     comments = data.place.comments.map(c => {
