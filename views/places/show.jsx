c56feb069ebfc9899e19ccd5d276e9fcd819611c
Tymari Veloso <tymariveloso@gmail.com>
2022-12-15 22:09:29 -0800

    added method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,22 +5,43 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <img src={data.place.pic} alt={data.place.name} height={500} width={500}/>
-            <h2>Rating</h2>
-            <p>Not Rated</p>
-            <h2>Description</h2>
-            <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
-            <h2>Comments</h2>
-            <p>No comments yet!</p>
-            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-              Edit
-            </a>
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-              <button type='submit' className='btn btn-danger'>
-                Delete
-              </button>
-            </form>
+            <div className='row'>
+              <div className='col-sm-6'>
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className='col-sm-6'>
+                <h1>
+                  {data.place.name}
+                </h1>
+                <h2>
+                  Rating
+                </h2>
+                <p>Not Rated</p>
+                <h2>
+                  Description
+                </h2>
+                <p>Not Rated</p>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+                <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                  Edit
+                </a>
+                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                <button type='submit' className='btn btn-danger'>
+                  Delete
+                </button>
+                </form>
+              </div>
+            </div>
           </main>
         </Def>
     )
